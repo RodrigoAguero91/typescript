5bec4a676ee4489649551be5928d90e0a443db9d
Rodrigo Aguero <aguerorodrigo91@gmail.com>
2026-05-14 20:45:25 -0300

    creando carpeta pages

--- src/pages/admin/home.ts
+++ src/pages/admin/home.ts
@@ -1,4 +1,4 @@
-import { checkAuhtUser, logout } from "../../../utils/auth";
+import { checkAuhtUser, logout } from "../../utils/auth";
 
 const buttonLogout = document.getElementById(
   "logoutButton"
